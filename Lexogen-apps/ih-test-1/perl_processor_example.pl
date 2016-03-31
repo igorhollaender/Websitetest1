29a6413627511ebda7b456d7c1965cbe2d4aa420
Igor Hollaender (Lexogen) <igor.hollaender@lexogen.com>
2016-03-31 15:39:37 +0200

    added commentary og wp page format

--- Lexogen-apps/ih-test-1/perl_processor_example.pl
+++ Lexogen-apps/ih-test-1/perl_processor_example.pl
@@ -12,6 +12,49 @@
 #
 #################################################################################################
 
+#################################################################################################
+#
+#       The WordPress page using this script ahs the following content:
+#	(using the 'Insert HP' plugin)
+#
+#################################################################################################
+#
+#	[insert_php]
+#	
+#	$descriptorspec = array(
+#		0 => array("pipe", "r"),  
+#		1 => array("pipe", "w"),
+#		2 => array("file", "/home/ubuntu/workspace/Lexogen-apps/ih-test-1/error-output.txt", "a")
+#	);
+#	
+#	$cwd = '/home/ubuntu/workspace/Lexogen-apps/ih-test-1/';
+#	$env = array('some_option' => 'aeiou');
+#	
+#	$process = proc_open(
+#		'/home/ubuntu/workspace/Lexogen-apps/ih-test-1/perl_processor_example.pl', 
+#		$descriptorspec, 
+#		$pipes, $cwd, $env);
+#	
+#	if (is_resource($process)) {
+#		fwrite($pipes[0], '');
+#		fclose($pipes[0]);
+#	
+#		//  IH160331 for this to work, we had to make a change in 
+#		//  wp-includes/post-template.php
+#		// (see https://core.trac.wordpress.org/ticket/27134)
+#	
+#		echo stream_get_contents($pipes[1]);
+#
+#		fclose($pipes[1]);
+#	
+#		$return_value = proc_close($process);
+#	
+#		echo "\n\ncommand returned $return_value\n";
+#		}
+#	
+#	[/insert_php]
+#
+#################################################################################################
 
 print "I AM PERL PROCESSOR generating SVG output (Ver.160331b)\n";
 my $now_string = localtime;
